e5b73f7e3e3628a8752c413811008ec5156b9509
Aakash Saravanan <aakash@Aakashs-MacBook-Pro.local>
2025-07-22 00:28:47 -0700

    added changes to sql parser

--- parser/sql_parser.py
+++ parser/sql_parser.py
@@ -1,12 +1,97 @@
 from planner.logical_plan import LogicalPlan
 
-def valid_format(sql_text: str) -> bool:
+def valid_format(sql_text: str) -> LogicalPlan | None:
+    if not sql_text:
+        return False
     
-    return True
+    sql_text = sql_text.strip().upper()
+    
+    if sql_text.endswith(";"):
+        sql_text = sql_text[:-1]
+
+    tokens = sql_text.split()
+
+    if not tokens[0] == "SELECT":
+        return False
+    
+    try:
+        sel_idx = tokens.index("SELECT") # required
+        from_idx = tokens.index("FROM") # required
+    except ValueError:
+        return None
+    
+    try:
+        where_idx = tokens.index("WHERE") # optional
+    except ValueError:
+        where_idx = -1
+
+    try:
+        oby_idx = tokens.index("ORDER") # optional
+        if tokens[oby_idx + 1] != "BY":
+            return None
+    except (ValueError, IndexError):
+        oby_idx = -1
+
+    try:
+        oby_dir_idx = tokens.index("ASC") # optional
+        order_by_dir = "ASC"
+    except ValueError:
+        try:
+            oby_dir_idx = tokens.index("DESC") # optional
+            order_by_dir = "DESC"
+        except ValueError:
+            oby_dir_idx = -1
+            order_by_dir = None
+    
+    col_proj = tokens[sel_idx + 1 : from_idx] # get column projections
+    if not col_proj:
+        return False
+    
+    # get source tables
+    if where_idx == -1 and oby_idx == -1:
+        source_tables = tokens[from_idx + 1 : len(tokens)]
+    elif where_idx == -1:
+        source_tables = tokens[from_idx + 1 : oby_idx]
+    else:
+        source_tables = tokens[from_idx + 1 : where_idx]
+    
+    if not source_tables:
+        return False
+
+    # get filters
+    filter_clause = None
+    if where_idx != -1:
+        if oby_idx == -1:
+            filter_clause = " ".join(tokens[where_idx + 1: len(tokens)])
+        else:
+            filter_clause = " ".join(tokens[where_idx + 1: oby_idx])
+
+    # get order by
+    order_by = None
+    if oby_idx != -1:
+        if oby_dir_idx == -1:
+            order_by = tokens[oby_idx + 2 : oby_dir_idx]
+        else:
+            order_by = tokens[oby_idx + 2 : len(tokens)]
+
+    return LogicalPlan(col_proj=col_proj, source_tables=source_tables, 
+                        filter=filter_clause, order_by=order_by, order_dir=order_by_dir)
+
+def table_exists(table) -> bool:
     pass
 
+def typecheck(plan) -> bool:
+    if not plan:
+        return False
 
-def typecheck(sql_text):
+    source = plan.source_tables
+    # check whether each table exists in our database
+    for table in source:
+        if not table_exists(table):
+            return False
+    
+    columns = plan.col_proj
+
     pass
 
 def parse_query(sql_text: str) -> LogicalPlan:
@@ -19,12 +104,11 @@ def parse_query(sql_text: str) -> LogicalPlan:
     Returns:
         LogicalPlan: root of logical operator tree
     """
-
-    if not valid_format(sql_text):
+    logical_plan = valid_format(sql_text)
+    if not logical_plan:
         raise ValueError("Invalid query format")
 
-    if not typecheck(sql_text):
+    if not typecheck(logical_plan):
         raise ValueError("SQL query failed type checks")
 
     # TODO: Tokenize SQL and construct logical operator tree
-
--- planner/logical_plan.py
+++ planner/logical_plan.py
@@ -1,3 +1,16 @@
 class LogicalPlan:
-    def __init__(self):
-        self.node_id = 0
+    def __init__(self, col_proj=None, source_tables=None, filter=None, order_by=None, order_dir=None):
+        self.col_proj = col_proj
+        self.source_tables = source_tables
+        self.filter = filter
+        self.order_by = order_by
+        self.order_dir = order_dir
+    
+    def __repr__(self):
+        return (f"LogicalPlan(\n"
+                f"  col_proj={self.col_proj},\n"
+                f"  source_tables={self.source_tables},\n"
+                f"  filter={self.filter},\n"
+                f"  order_by={self.order_by},\n"
+                f"  order_dir={self.order_dir}\n"
+                f")")
